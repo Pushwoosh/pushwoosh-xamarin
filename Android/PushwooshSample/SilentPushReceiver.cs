40ddbb8e396acfb0ce4fcf337558dd42f146dcc0
diman <diman@pushwoosh.com>
2015-09-10 14:22:48 +0600

    Silent push receiver sample

---
+++ Android/PushwooshSample/SilentPushReceiver.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Util;
+using Pushwoosh;
+
+namespace PushwooshSample
+{
+	[BroadcastReceiver(Enabled=true, Permission=".permission.C2D_MESSAGE")]
+	[IntentFilter(new[] { "com.pushwoosh.test.xamarin.app.action.SILENT_PUSH_RECEIVE" })]
+	public class SilentPushReceiver : BroadcastReceiver
+	{
+		public override void OnReceive (Context context, Intent intent)
+		{
+			Log.Debug ("PushwooshSample", intent.GetStringExtra(BasePushMessageReceiver.JsonDataKey));
+		}
+	}
+}
+
