9463403b779f213491f6fe31933f863f4f37f4ce
diman <diman@pushwoosh.com>
2016-05-19 15:13:05 +0600

    Pushwoosh Xamarin.Forms sample

---
+++ Forms/Droid/MainActivity.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+using Pushwoosh;
+
+namespace PushwooshSample.Droid
+{
+	class LocalMessageBroadcastReceiver : BasePushMessageReceiver
+	{
+		public MainActivity activity {get; set;}
+
+		protected override void OnMessageReceive (Intent intent)
+		{
+			activity.doOnMessageReceive (intent.GetStringExtra (BasePushMessageReceiver.JsonDataKey));
+		}
+	}
+
+	[Activity (Label = "PushwooshSample.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		LocalMessageBroadcastReceiver mMessageReceiver;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			LoadApplication (new App ());
+
+			mMessageReceiver = new LocalMessageBroadcastReceiver ();
+			mMessageReceiver.activity = this;
+
+			registerReceivers ();
+
+			PushManager manager = PushManager.GetInstance (this);
+			manager.OnStartup (this);
+
+			// Register for push!
+			manager.RegisterForPushNotifications();
+
+			// Reset application icon badge number
+			manager.BadgeNumber = 0;
+
+			checkMessage (Intent);
+		}
+
+		protected override void OnNewIntent(Intent intent)
+		{
+			checkMessage (intent);
+		}
+
+		public void checkMessage(Intent intent)
+		{
+			if (null != intent)
+			{
+				if (intent.HasExtra(PushManager.PushReceiveEvent))
+				{
+					doOnMessageReceive(intent.Extras.GetString(PushManager.PushReceiveEvent));
+				}
+
+				resetIntentValues();
+			}
+		}
+			
+		public void doOnMessageReceive(String message)
+		{
+			// hadle push open here
+			Log.Debug("PushwooshSample", "Push opened: " + message);
+		}
+
+		private void resetIntentValues()
+		{
+			Intent mainAppIntent = Intent;
+
+			if (mainAppIntent.HasExtra(PushManager.PushReceiveEvent))
+			{
+				mainAppIntent.RemoveExtra(PushManager.PushReceiveEvent);
+			}
+			else if (mainAppIntent.HasExtra(PushManager.RegisterEvent))
+			{
+				mainAppIntent.RemoveExtra(PushManager.RegisterEvent);
+			}
+			else if (mainAppIntent.HasExtra(PushManager.UnregisterEvent))
+			{
+				mainAppIntent.RemoveExtra(PushManager.UnregisterEvent);
+			}
+			else if (mainAppIntent.HasExtra(PushManager.RegisterErrorEvent))
+			{
+				mainAppIntent.RemoveExtra(PushManager.RegisterErrorEvent);
+			}
+			else if (mainAppIntent.HasExtra(PushManager.UnregisterErrorEvent))
+			{
+				mainAppIntent.RemoveExtra(PushManager.UnregisterErrorEvent);
+			}
+
+			Intent = mainAppIntent;
+		}
+
+		protected override void OnResume ()
+		{
+			base.OnResume ();
+
+			registerReceivers ();
+		}
+
+		protected override void OnPause ()
+		{
+			base.OnPause ();
+
+			unregisterReceivers ();
+		}
+
+		public void registerReceivers()
+		{
+			IntentFilter intentFilter = new IntentFilter(PackageName + ".action.PUSH_MESSAGE_RECEIVE");
+			RegisterReceiver(mMessageReceiver, intentFilter);
+		}
+
+		public void unregisterReceivers()
+		{
+			UnregisterReceiver(mMessageReceiver);
+		}
+	}
+}
+
--- Forms/iOS/AppDelegate.cs
+++ Forms/iOS/AppDelegate.cs
@@ -1,35 +1,21 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Foundation;
-using UIKit;
 using Pushwoosh;
 
-namespace PushwooshSample
+using Foundation;
+using UIKit;
+
+namespace PushwooshSample.iOS
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the 
-	// User Interface of the application, as well as listening (and optionally responding) to 
-	// application events from iOS.
 	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
-		// class-level declarations
-		UIWindow window;
-		PushwooshSampleViewController viewController;
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this 
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
-			window = new UIWindow (UIScreen.MainScreen.Bounds);
-			
-			viewController = new PushwooshSampleViewController ();
-			window.RootViewController = viewController;
-			window.MakeKeyAndVisible ();
+			global::Xamarin.Forms.Forms.Init ();
+
+			LoadApplication (new App ());
 
 			PushNotificationManager pushmanager = PushNotificationManager.PushManager;
 			pushmanager.Delegate = this;
@@ -47,7 +33,7 @@ namespace PushwooshSample
 
 			Console.WriteLine("HWID: " + pushmanager.GetHWID);
 
-			return true;
+			return base.FinishedLaunching (app, options);
 		}
 
 		public override void RegisteredForRemoteNotifications (UIApplication application, NSData deviceToken)
@@ -66,6 +52,5 @@ namespace PushwooshSample
 			PushNotificationManager.PushManager.HandlePushReceived (userInfo);
 		}
 	}
-
 }
 
