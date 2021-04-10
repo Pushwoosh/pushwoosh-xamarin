c574d7fa363f59e5c00e448ebacb1e691b857565
Nikolay Galizin <ngalizin@pushwoosh.com>
2021-04-10 19:36:05 +0700

    iOS Bindings 5.23.1. Android Bindings 5.23.0. Library 5.24.0.

--- Inbox/Bindings/Android/Properties/AssemblyInfo.cs
+++ Inbox/Bindings/Android/Properties/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using Android.App;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("5.22.6")]
+[assembly: AssemblyVersion("5.23.0")]
--- Inbox/Library/Pushwoosh.Inbox.Droid/Properties/AssemblyInfo.cs
+++ Inbox/Library/Pushwoosh.Inbox.Droid/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using Android.App;
 [assembly: ComVisible(false)]
 
 
-[assembly: AssemblyVersion("5.23.1")]
+[assembly: AssemblyVersion("5.24.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
